5f22ba066c20e821ab0b0f09ecef93442f90705f
anita <anita@fillygaming888.com>
2022-10-20 17:35:37 +0800

    Delete book

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,8 +4,17 @@
       class="book-item"
       v-for="book in store.books"
       :key="book.id"
-      @click="$router.push({name: routesName.book, params: { id: book.id }})"
     >
+      <div class="book-item__cover">
+        <span 
+          className="material-icons book-item__read-more"
+          @click="$router.push({name: routesName.book, params: { id: book.id }})"
+        >read_more</span>
+        <span 
+          className="material-icons book-item__delete"
+          @click="delHandler(book.id)"
+        >delete</span>
+      </div>
       <h3 class="book-item__title">{{book.title}}</h3>
       <h4 class="book-item__author">Author: {{book.author}}</h4>
       <p class="book-item__description">{{book.description}}</p>
@@ -19,6 +28,11 @@ import { useBooksStore } from '../stores/books';
 
 const store = useBooksStore();
 await store.fetchBooks();
+
+const delHandler = async (bookId: number) => {
+  await store.delBook(bookId);
+  await store.fetchBooks();
+}
 </script>
 
 <style lang="scss" scoped>
@@ -27,8 +41,15 @@ await store.fetchBooks();
 }
 
 .book-item {
-  @apply p-4 bg-black rounded-md border border-transparent;
-  transition: border-color 0.25s;
+  @apply p-4 bg-black rounded-md border border-transparent relative transition-colors overflow-hidden;
+  &__cover {
+    @apply w-full h-full flex justify-center items-center space-x-6;
+    @apply absolute right-0 bottom-0 p-2 opacity-0 pointer-events-none transition-opacity;
+    @apply bg-gray-900 bg-opacity-80;
+    span {
+      @apply hover:text-primary transition-colors;
+    }
+  }
   &__title {
     @apply text-xl mb-1 truncate;
   }
@@ -45,6 +66,9 @@ await store.fetchBooks();
   }
   &:hover {
     @apply border-primary cursor-pointer;
+    .book-item__cover {
+      @apply opacity-100 pointer-events-auto;
+    }
   }
 }
 
